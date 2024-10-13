f0a309c8010f01e93e9ca06eaaf99070dafe9592
Roydags <rddaguplo.cbsd@gmail.com>
2024-10-13 10:00:20 +0800

    first commit

---
+++ test.sql
@@ -0,0 +1,16 @@
+CREATE TABLE employees(
+id INT AUTO_INCREMENT,
+   first_name VARCHAR(100),
+   last_name VARCHAR(100),
+   age INT,
+   position VARCHAR(100),
+   address VARCHAR(255),
+   PRIMARY KEY(id)
+);
+
+
+INSERT INTO employees (first_name, last_name, age, position, address) values ('Roy', 'Dags', 24, 'Enumerator', 'Dipolog'), ('Nancy', 'Pals', 26, 'Supervisor', 'Zamboanga'), ('Rovick', 'Daps', 28, 'Analyst', 'Labason');
+
+
+
+
